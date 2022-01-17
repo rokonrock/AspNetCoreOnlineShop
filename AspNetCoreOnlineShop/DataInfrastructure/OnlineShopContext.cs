63e966b16102ad9af28b2ac8c8a89a8b04767fc8
Abdullah Al Mamun <abdullah.almamun@inmarsat.com>
2022-01-16 20:37:27 -0330

    DbContext

---
+++ AspNetCoreOnlineShop/DataInfrastructure/OnlineShopContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreOnlineShop.DataInfrastructure
+{
+    public class OnlineShopContext : DbContext
+    {
+        public OnlineShopContext(DbContextOptions<OnlineShopContext> options) : base(options)
+        {
+        }
+
+
+
+    }
+}
